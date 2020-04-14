2339f733261d56320b17ecef1c928a62163bd7ff
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-04-14 13:28:04 +0200

    Initialisation du fichier SQL sur les droits
    Initialisation du fichier SQL sur les droits concernant les schémas de la base de données.

---
+++ sql/orga_donnees_infogeo_99_droit.sql
@@ -0,0 +1,6 @@
+/* ORGA_DONNEES_INFOGEO V1.0 */
+/* Creation des droits sur l'ensemble des objets */
+/* orga_donnees_infogeo_99_droit.sql */
+/* PostgreSQL/PostGIS */
+/* Conseil régional Nouvelle-Aquitaine - https://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT */
