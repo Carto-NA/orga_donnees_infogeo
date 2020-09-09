34ca8d779b7634f7a9411d8dd565ed727caa5532
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-09-09 15:43:42 +0200

    Update and rename orga_donnees_fonctionnel.sql to orga_donnees_infogeo_80_fonctionnel.sql

---
+++ sql/orga_donnees_infogeo_80_fonctionnel.sql
@@ -0,0 +1,30 @@
+### Requêtes utilies pour le fonctionnement
+
+
+-- Requête pour extraire les commentaires sur les schémas
+SELECT 
+  t1.*, t2.description
+FROM
+(
+  SELECT 
+    table_schema, table_name, table_type 
+  FROM 
+    information_schema.tables 
+  WHERE table_schema NOT IN ('information_schema','pg_catalog')
+) t1
+INNER JOIN 
+(
+  SELECT
+    pg_class_objoid.relname AS "Table/View Name", pg_description.*
+  FROM
+    pg_description
+  LEFT JOIN
+    pg_class AS pg_class_objoid ON pg_description.objoid = pg_class_objoid.oid
+  LEFT JOIN
+    pg_class AS pg_class_classoid ON pg_description.classoid = pg_class_classoid.oid
+  WHERE
+    pg_class_classoid.relname = 'pg_class' 
+) t2
+ON 
+  t2."Table/View Name"!='tcl_sytral.tcllignemf' AND t1.table_name=t2."Table/View Name"
+ORDER BY t1.table_schema, t1.table_name ASC;
