70e560605d278ada7fca4c102741e153628ed6bb
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2022-12-12 10:18:27 +0100

    Create 00_jeux_classes_entites.py
    Initialisation du fichier

---
+++ metier/00_jeux_classes_entites.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        00_jeux_classes_entites
+# Purpose:     Script qui permet de gérer les jeux de classes d'entités dans la geodatabase ESRI
+#
+# Author:      vincentto
+#
+# Created:     12/12/2022
+# Copyright:   (c) vincentto 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# coding: utf-8
